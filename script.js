b6d2f84384ac56cab41ffbff014569b72fec3469
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2020-11-19 14:34:43 -0500

    email
    email function to the form

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+// function emailLeads() {
+// if (document.getElementById('list').value == "Bev Smith Kia’s Reprocessed Car Sale & Trade In Event") {
+//   var email = document.createElement('a');
+//   email.href = "mailto:gannjeremy644@gmail.com";
+//   Mail.click();
+// }
+
+
+
+function sendMail() {
+  if(document.getElementById("list").value == "Bev Smith Kia’s Reprocessed Car Sale & Trade In Event"){
+  var link = "mailto:wesmckeehan@profitswithadvertising.com"
+           + "?cc=jeremy@profitswithadvertising.com"
+           + "&subject=" + encodeURIComponent("PWA Leads")
+           + "&body=" + encodeURIComponent(document.getElementById('firsty').value) + "       "
+           + encodeURIComponent(document.getElementById('lastName').value) + "    " 
+           + encodeURIComponent(document.getElementById('Phone').value) + "     "
+           + encodeURIComponent(document.getElementById('Email').value)  +  "     " 
+           + encodeURIComponent(document.getElementById('Date').value) + "     "
+           + encodeURIComponent(document.getElementById('Appt-Time').value)  
+          
+  ;
+  
+  window.location.href = link;
+}
+
+else {
+  alert("select right event!");
+}
+}
